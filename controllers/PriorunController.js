99c93509a904a8bff86ddb57cb9ace255149c33f
Sebbo92 <pohl.sebastian92@googlemail.com>
2020-08-08 14:07:19 +0200

    export all functions

--- controllers/PriorunController.js
+++ controllers/PriorunController.js
@@ -1,7 +1,13 @@
 "use strict";
 
-exports.generateRandomId = function () {
+module.exports = {
+    generateRandomId
+  }
+
+
+function generateRandomId() {
     const minId = 1000;
     const maxId = 9999;
+    
     return Math.floor(Math.random() * (maxId - minId + 1)) + minId;
 }
